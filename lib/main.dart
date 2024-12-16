37a4609c44f9f401490d49be25523c703be07e03
Peter428 <peterlase28@gmail.com>
2024-12-16 10:01:10 +0700

    row & coloum

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(new MaterialApp(
+    title: "Material App",
     home: new Index(),
   ));
 }
@@ -10,20 +11,22 @@ class Index extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: 
-      new Center(
-        child: new Container(
-        color: Colors.blue[900],
-        width: 100.0,
-        height: 100.0,
-        child: new Center(
-            child: new Icon(Icons.apple, 
-              color: Colors.white,
-              size: 70.0,
-              semanticLabel: "Apple",
-            ),
-          ),
-        )),
+      appBar: new AppBar(
+        backgroundColor: Colors.green,
+        leading: new Icon(Icons.home),
+        title: new Center(child: new Text("Material App")),
+        actions: <Widget>[
+          new Icon(Icons.search),
+        ],
+      ),
+      body: new Container(
+          child: new Row(children: <Widget>[
+        new Icon(
+          Icons.local_pizza,
+          size: 100.0,
+          color: Colors.green,
+        )
+      ])),
     );
   }
 }
